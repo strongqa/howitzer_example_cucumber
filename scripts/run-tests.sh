c755e6a3e41e3d1961f1d863ead2e749fc368b77
Roman Parashchenko <romikoops1@gmail.com>
2016-08-05 23:41:17 +0200

    fixed travis config

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 bundle exec rake features:smoke
 shopt -s nocasematch;
-if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs.* ]]
+if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist.* ]]
 then
 	bundle exec rake features:bvt features:p1 features:p2
 fi

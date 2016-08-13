591bb05d872c0dbf63d37e361e16c977c1a335e2
greenhairedlime <kalinichenko1masha@gmail.com>
2016-08-13 22:14:57 +0300

    refactored cache and logging

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,9 +7,9 @@ World(FactoryGirl::Syntax::Methods)
 
 FileUtils.mkdir_p(Howitzer.log_dir)
 
-log.settings_as_formatted_text
-cache.store(:cloud, :start_time, Time.now.utc)
-cache.store(:cloud, :status, true)
+Howitzer::Log.settings_as_formatted_text
+Howitzer::Cache.store(:cloud, :start_time, Time.now.utc)
+Howitzer::Cache.store(:cloud, :status, true)
 
 if cloud_driver?
   Capybara.drivers[Howitzer.driver.to_sym][].options[:desired_capabilities][:name] = suite_name
@@ -17,27 +17,27 @@ if cloud_driver?
 end
 
 Before do |scenario|
-  log.print_feature_name(scenario.feature.name)
-  log.print_scenario_name(scenario.name)
-  @session_start = duration(Time.now.utc - cache.extract(:cloud, :start_time))
+  Howitzer::Log.print_feature_name(scenario.feature.name)
+  Howitzer::Log.print_scenario_name(scenario.name)
+  @session_start = duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
 end
 
 After do |scenario|
   if cloud_driver?
-    cache.store(:cloud, :status, false) if scenario.failed?
-    session_end = duration(Time.now.utc - cache.extract(:cloud, :start_time))
-    log.info "CLOUD VIDEO #{@session_start} - #{session_end}" \
+    Howitzer::Cache.store(:cloud, :status, false) if scenario.failed?
+    session_end = duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
+    Howitzer::Log.info "CLOUD VIDEO #{@session_start} - #{session_end}" \
              " URL: #{cloud_resource_path(:video)}"
   elsif ie_browser?
-    log.info 'IE reset session'
+    Howitzer::Log.info 'IE reset session'
     page.execute_script("void(document.execCommand('ClearAuthenticationCache', false));")
   end
-  cache.clear_all_ns
+  Howitzer::Cache.clear_all_ns
 end
 
 at_exit do
   if cloud_driver?
-    log.info "CLOUD SERVER LOG URL: #{cloud_resource_path(:server_log)}"
-    update_cloud_job_status(passed: cache.extract(:cloud, :status))
+    Howitzer::Log.info "CLOUD SERVER LOG URL: #{cloud_resource_path(:server_log)}"
+    update_cloud_job_status(passed: Howitzer::Cache.extract(:cloud, :status))
   end
 end

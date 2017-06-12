2426e3148f38ea5d02ef7f1ada571fb8fd34c8df
Roman Parashchenko <romikoops1@gmail.com>
2017-06-12 20:05:48 +0300

    Headless chrome ci test (#3)
    * travis config changed, test script changed
    
    * config update

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 bundle exec rake rubocop features:smoke
 shopt -s nocasematch;
-if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist.* ]]
+if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist|headless_chrome.* ]]
 then
 	bundle exec rake features:bvt features:p1 features:p2
 fi

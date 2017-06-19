c0cc6c51cc0545cfd47231b44b0a01cfc542176a
Alexey Voronenko <alexey.voronenko96@gmail.com>
2017-06-19 10:22:27 +0300

    Webkit travisci feature (#5)
    * webkit gem and ci support
    
    * gemnasium gem versions update
    
    * circleci gem installation fix
    
    * rubocop corrections, nokogiri gem update
    
    * travis ci builds optimization
    
    * script changes
    
    * script changes
    
    * rubocop config update

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 set -ev
+if [[ "$SEXY_SETTINGS" =~ .*headless_chrome.* ]]
+then
+    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
+    sudo cp chromedriver /usr/local/bin/chromedriver
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start &
+    sleep 3
+fi
+if [[ "$SEXY_SETTINGS" =~ .*webkit.* ]]
+then
+    sudo apt-get install qt5-default libqt5webkit5-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start &
+    sleep 3
+fi
 bundle exec rake rubocop features:smoke
 shopt -s nocasematch;
-if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist|headless_chrome.* ]]
+if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist|headless_chrome|webkit.* ]]
 then
 	bundle exec rake features:bvt features:p1 features:p2
 fi

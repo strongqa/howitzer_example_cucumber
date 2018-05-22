02d4880221468c18bf0959dbb78a0c9aad1ea1b4
Kapustochka <maximkalavrenenko@gmail.com>
2018-05-23 02:13:58 +0300

    Upgrade gems#13 (#15)
    * Version of chromedriver was changed in run-tests.sh
    
    * gemfile.lock was updated (bundle lock --update)
    
    * poltergeist and phantomjs jobs were removed(commented)
    
    * comments were deleted
    
    * downloadind of packages (qt) was written in main config under apt section instead of using apt-get in script
    
    * comments are deleted
    
    * poltergeist was returned
    
    * downgrade poltergeist library
    
    * minor
    
    * updated config submodule
    
    * ruby version was changed from 2.4.0 to 2.5.1

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,14 +2,13 @@
 set -ev
 if [[ "$SEXY_SETTINGS" =~ .*headless_chrome.* ]]
 then
-    wget https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
+    wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
     sudo cp chromedriver /usr/local/bin/chromedriver
     sleep 3
 fi
 if [[ "$SEXY_SETTINGS" =~ .*webkit.* ]]
 then
-    sudo apt-get install qt5-default libqt5webkit5-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start &
     sleep 3
@@ -21,7 +20,7 @@ else
     bundle exec rake rubocop features:smoke
 fi
 shopt -s nocasematch;
-if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*phantomjs|poltergeist|headless_chrome|webkit.* ]]
+if [[ "$SEXY_SETTINGS" == "" || "$SEXY_SETTINGS" =~ .*poltergeist|headless_chrome|webkit.* ]]
 then
 	bundle exec rake features:bvt features:p1 features:p2
 fi

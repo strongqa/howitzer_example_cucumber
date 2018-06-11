7d2017eaab7d573956ac28f87dfd84ec2b5ee2df
Maxim <maximkalavrenenko@gmail.com>
2018-06-11 12:08:57 +0300

    exception handling was removed from hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,40 +1,28 @@
 Before do |scenario|
-  begin
-    Capybara.use_default_driver
-    Howitzer::Log.print_feature_name(scenario.feature.name)
-    Howitzer::Log.print_scenario_name(scenario.name)
-    @session_start = CapybaraHelpers.duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
-  rescue StandardError => e
-    Howitzer::Log.error("Before scenarion hook: #{e.message}")
-  end
+  Capybara.use_default_driver
+  Howitzer::Log.print_feature_name(scenario.feature.name)
+  Howitzer::Log.print_scenario_name(scenario.name)
+  @session_start = CapybaraHelpers.duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
 end
 
 After do |scenario|
-  begin
-    if CapybaraHelpers.cloud_driver?
-      Howitzer::Cache.store(:cloud, :status, false) if scenario.failed?
-      session_end = CapybaraHelpers.duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
-      Howitzer::Log.info "CLOUD VIDEO #{@session_start} - #{session_end}" \
-               " URL: #{CapybaraHelpers.cloud_resource_path(:video)}"
-    elsif CapybaraHelpers.ie_browser?
-      Howitzer::Log.info 'IE reset session'
-      Capybara.current_session.execute_script("void(document.execCommand('ClearAuthenticationCache', false));")
-    end
-    Howitzer::Cache.clear_all_ns
-    Capybara.reset_sessions!
-  rescue StandardError => e
-    Howitzer::Log.error("After scenario hook: #{e.message}")
+  if CapybaraHelpers.cloud_driver?
+    Howitzer::Cache.store(:cloud, :status, false) if scenario.failed?
+    session_end = CapybaraHelpers.duration(Time.now.utc - Howitzer::Cache.extract(:cloud, :start_time))
+    Howitzer::Log.info "CLOUD VIDEO #{@session_start} - #{session_end}" \
+             " URL: #{CapybaraHelpers.cloud_resource_path(:video)}"
+  elsif CapybaraHelpers.ie_browser?
+    Howitzer::Log.info 'IE reset session'
+    Capybara.current_session.execute_script("void(document.execCommand('ClearAuthenticationCache', false));")
   end
+  Howitzer::Cache.clear_all_ns
+  Capybara.reset_sessions!
 end
 
 at_exit do
-  begin
-    if CapybaraHelpers.cloud_driver?
-      Howitzer::Log.info "CLOUD SERVER LOG URL: #{CapybaraHelpers.cloud_resource_path(:server_log)}"
-      CapybaraHelpers.update_cloud_job_status(passed: Howitzer::Cache.extract(:cloud, :status))
-    end
-  rescue StandardError => e
-    Howitzer::Log.error("At exit hook: #{e.message}")
+  if CapybaraHelpers.cloud_driver?
+    Howitzer::Log.info "CLOUD SERVER LOG URL: #{CapybaraHelpers.cloud_resource_path(:server_log)}"
+    CapybaraHelpers.update_cloud_job_status(passed: Howitzer::Cache.extract(:cloud, :status))
   end
 end
 
--- tasks/cuke_sniffer.rake
+++ tasks/cuke_sniffer.rake
@@ -8,13 +8,14 @@ namespace :cuke_sniffer do
     CukeSniffer::RuleConfig::RULES[:too_many_tags][:enabled] = false
     CukeSniffer::RuleConfig::RULES[:multiple_given_when_then][:enabled] = false
     CukeSniffer::RuleConfig::RULES[:out_of_order_steps][:enabled] = false
-    # CukeSniffer::RuleConfig::RULES[:too_many_steps][:max] = 15
+    CukeSniffer::RuleConfig::RULES[:too_many_steps][:max] = 15
 
     cuke_sniffer = CukeSniffer::CLI.new(
       project_location: './',
       no_catalog: false
     )
-    # results = cuke_sniffer.output_results
+    cuke_sniffer.output_results
+    cuke_sniffer.output_html
     unless cuke_sniffer.summary[:improvement_list].empty?
       puts 'Improvements are needed'
       exit(150)

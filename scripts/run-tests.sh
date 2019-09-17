9ab4ac40b0770e05bcedb21c491a41e118edaec1
Vitalii Franchuk <fravit4@gmail.com>
2019-09-17 16:16:27 +0300

    headless_chrome issue fix 5

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,6 @@
 set -ev
 if [[ "$SEXY_SETTINGS" =~ .*headless_chrome.* ]]
 then
-    sudo apt-get install dpkg -y
     wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
     sudo cp chromedriver /usr/local/bin/chromedriver

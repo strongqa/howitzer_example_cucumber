ec12b3784718acad061e7261b85250c8a129f373
Vitalii Franchuk <fravit4@gmail.com>
2019-09-17 14:32:59 +0300

    headless_chrome issue fix

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,10 @@ then
     unzip chromedriver_linux64.zip
     sudo cp chromedriver /usr/local/bin/chromedriver
     sleep 3
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub 9 | sudo apt-key add -
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ 6 stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
 fi
 if [[ "$SEXY_SETTINGS" =~ .*headless_firefox.* ]]
 then
